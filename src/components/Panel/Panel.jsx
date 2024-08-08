586476697fec4d2e101794da6c16281e74811e43
basesnel <basesnel@gmail.com>
2024-08-08 15:31:11 +0300

    Added styles to interactive Panels.

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -4,9 +4,21 @@ import styles from "./styles.module.css";
 
 const Panel = ({ title, children, isActive, onShow }) => {
   return (
-    <form className={styles.panel}>
-      <h3>{title}</h3>
-      {isActive ? <>{children}</> : <button onClick={onShow}>Show</button>}
+    <form
+      className={isActive ? styles.panel : `${styles.panel} ${styles.dark}`}
+    >
+      <h3
+        className={isActive ? styles.title : `${styles.title} ${styles.light}`}
+      >
+        {title}
+      </h3>
+      {isActive ? (
+        <>{children}</>
+      ) : (
+        <button className={styles.button} onClick={onShow}>
+          show
+        </button>
+      )}
     </form>
   );
 };

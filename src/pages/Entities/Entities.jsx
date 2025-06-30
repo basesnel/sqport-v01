aad73d7d6f43cb6d640fe7e98d2991c1dc385eb4
basesnel <basesnel@gmail.com>
2025-06-30 21:23:57 +0300

    Clean up Entities.jsx from comments.

--- src/pages/Entities/Entities.jsx
+++ src/pages/Entities/Entities.jsx
@@ -1,11 +1,6 @@
 import { useEffect } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-// import jsonSystems from "./jsons/systems.json";
-// import styles from "./styles.module.css";
-
-// const systems =
-//   '{"entity": "system", "developer": "Microsoft", "name": "Windows 95", "logo": "W-logo", "family": "Windows 9x", "version": "4.00.950", "release": "August 24, 1995", "packageManager": "Program manager", "platforms": "IA-32", "kernel": "Monolithic", "defaultInterface" : "Desktop, taskbar, Start menu and Windows Explorer file manager", "license": "Proprietary commercial software", "site": "Windows 95 (archived at Wayback Machine)"}';
 
 const Main = () => {
   useEffect(() => {

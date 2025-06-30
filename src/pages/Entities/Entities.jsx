28d4e1a06f23f6f98dface7ae59f9df19191d78e
basesnel <basesnel@gmail.com>
2025-06-30 21:01:15 +0300

    Added first JSON-file.

--- src/pages/Entities/Entities.jsx
+++ src/pages/Entities/Entities.jsx
@@ -4,13 +4,15 @@ import { useEffect } from "react";
 
 // import styles from "./styles.module.css";
 
-const systems =
-  '{"entity": "system", "developer": "Microsoft", "name": "Windows 95", "logo": "W-logo", "family": "Windows 9x", "version": "4.00.950", "release": "August 24, 1995", "packageManager": "Program manager", "platforms": "IA-32", "kernel": "Monolithic", "defaultInterface" : "Desktop, taskbar, Start menu and Windows Explorer file manager", "license": "Proprietary commercial software", "site": "Windows 95 (archived at Wayback Machine)"}';
+// const systems =
+//   '{"entity": "system", "developer": "Microsoft", "name": "Windows 95", "logo": "W-logo", "family": "Windows 9x", "version": "4.00.950", "release": "August 24, 1995", "packageManager": "Program manager", "platforms": "IA-32", "kernel": "Monolithic", "defaultInterface" : "Desktop, taskbar, Start menu and Windows Explorer file manager", "license": "Proprietary commercial software", "site": "Windows 95 (archived at Wayback Machine)"}';
 
 const Main = () => {
   useEffect(() => {
     try {
-      console.log(JSON.parse(systems));
+      fetch("src/jsons/systems.json")
+        .then((response) => response.json())
+        .then((value) => console.log(value));
     } catch (error) {
       console.log(error);
     }

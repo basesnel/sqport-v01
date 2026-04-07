abb0b36803399bd465067ebbf2e63c230f9c2857
basesnel <basesnel@gmail.com>
2026-04-07 11:21:08 +0300

    formatTimeAgo is corrected.

--- src/helpers/formatTimeAgo.ts
+++ src/helpers/formatTimeAgo.ts
@@ -15,8 +15,18 @@ const formatTimeAgo = (dateString: string): string => {
     return `${Math.floor(secondPast / 3600)}h ago`;
   }
 
-  const day = Math.floor(secondPast / 86400);
-  return day === 1 ? `${day} day ago` : `${day} days ago`;
+  if (secondPast <= 2630016) {
+    const day = Math.floor(secondPast / 86400);
+    return day === 1 ? `${day} day ago` : `${day} days ago`;
+  }
+
+  if (secondPast <= 31560192) {
+    const month = Math.floor(secondPast / 2630016);
+    return month === 1 ? `${month} month ago` : `${month} months ago`;
+  }
+
+  const year = Math.floor(secondPast / 31560192);
+  return year === 1 ? `${year} year ago` : `${year} years ago`;
 };
 
 export default formatTimeAgo;

88c8b89c5667f6cfd7ab1495d0e5fb0357776848
basesnel <basesnel@gmail.com>
2026-04-07 11:09:26 +0300

    Footer js -> ts.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import formatTimeAgo from "../../helpers/formatTimeAgo";
 import formatDate from "../../helpers/formatDate";
-// import Section from "../Section/Section";
 import Container from "../Container/Container";
 
 import styles from "./styles.module.css";
@@ -13,7 +12,7 @@ const Footer = () => {
         <p className={styles.extra}>Info-tool | {formatDate(new Date())}</p>
         <p className={styles.extra}>
           {`created ${formatTimeAgo(
-            "Sun Jul 21 2024 12:13:58 GMT+0300 (Eastern European Summer Time)"
+            "Sun Jul 21 2024 12:13:58 GMT+0300 (Eastern European Summer Time)",
           )}`}
         </p>
       </Container>

a76c8c56a8b239e80bae7903c477d6554dfa6bf5
basesnel <basesnel@gmail.com>
2026-04-07 09:31:52 +0300

    Project migrated to TypeScript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import SharedLayout from "./components/SharedLayout/SharedLayout";
+import SharedLayout from "./components/SharedLayout/SharedLayout.tsx";
 import Welcome from "./pages/Welcome/Welcome";
-import Main from "./pages/Main/Main";
-import NotFound from "./pages/NotFound/NotFound";
-import Entities from "./pages/Entities/Entities";
+// import Main from "./pages/Main/Main";
+// import NotFound from "./pages/NotFound/NotFound";
+// import Entities from "./pages/Entities/Entities";
 
 const App = () => {
   return (
@@ -11,9 +11,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Welcome />} />
-          <Route path="/main" element={<Main />} />
+          {/* <Route path="/main" element={<Main />} />
           <Route path="/entities" element={<Entities />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} /> */}
         </Route>
       </Routes>
     </>
